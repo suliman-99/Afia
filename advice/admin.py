d6d971435e771858fc2abcf765e4eb7b4957cac4
Suliman Awad <sulimanawadstudy@gmail.com>
2023-08-02 19:19:51 +0300

    upgrade admin interface

--- advice/admin.py
+++ advice/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from advice.models import Advice
 
-# Register your models here.
+
+@admin.register(Advice)
+class AdviceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content']
+    fields = ['id', 'content']
+    readonly_fields = ['id']
+    search_fields = ['id', 'content']
+
--- consultation/admin.py
+++ consultation/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from consultation.models import *
 
-# Register your models here.
+
+@admin.register(Consultation)
+class ConsultationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created_at', 'done']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created_at', 'done']
+
