d9afb0666e8fcf95afefb556eaf4f41a9c87399f
Suliman Awad <sulimanawadstudy@gmail.com>
2023-08-03 21:48:09 +0300

    set the psermission of city country specialization as IsAuthenticated like update profile

--- statics/views.py
+++ statics/views.py
@@ -1,19 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from statics.serializers import *
 from resources.filters import AllFieldsFilterBackend
 
 class CountryViewSet(ModelViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class CityViewSet(ModelViewSet):
     serializer_class = CitySerializer
     queryset = City.objects.all().select_related('country')
     filter_backends = [AllFieldsFilterBackend]
+    permission_classes = [IsAuthenticated]
 
 
 class SpecializationViewSet(ModelViewSet):
     serializer_class = SpecializationSerializer
     queryset = Specialization.objects.all()
+    permission_classes = [IsAuthenticated]
 
