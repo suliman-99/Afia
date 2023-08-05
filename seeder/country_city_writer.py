22d062b1163b6bbf442c6ea49d7de2a6ff93b3bc
Suliman Awad <sulimanawadstudy@gmail.com>
2023-08-06 02:01:59 +0300

    update writing csv file

--- seeder/country_city_writer.py
+++ seeder/country_city_writer.py
@@ -1,6 +1,9 @@
+import pandas
 import geonamescache
 from unidecode import unidecode as _
 gc = geonamescache.GeonamesCache()
+
+# ----------------------------------------------------------------------------------------
 
 countries_names = []
 cities = []
@@ -9,25 +12,36 @@ for city_id, city_info in gc.get_cities().items():
 
     country_name = _(gc.get_countries().get(city_info.get('countrycode')).get('name'))
     city_name = _(city_info.get('name'))
-
-    if ',' in city_name or ',' in country_name:
-        continue
     
     countries_names.append(country_name)
     cities.append((city_name, country_name))
 
+# ----------------------------------------------------------------------------------------
 
 countries_names = list(set(countries_names))
+
+countries_data = {}
+countries_data['name'] = []
+
+for country_name in countries_names:
+    countries_data['name'].append(country_name)
+    
+country_file = pandas.DataFrame(countries_data)
+country_file.to_csv('country.csv', index=False)
+
+# ----------------------------------------------------------------------------------------
+
 cities = list(set(cities))
 
-with open('Country.csv', 'w', encoding='utf-8') as country_file:
-    country_file.write('name\n')
-    for country_name in countries_names:
-        country_file.write(country_name + '\n')
+city_data = {}
+city_data['name'] = []
+city_data['country_id'] = []
 
-with open('City.csv', 'w', encoding='utf-8') as city_file:
-    city_file.write('name,country_id\n')
-    for city in cities:
-        city_name = city[0]
-        country_name = city[1]
-        city_file.write(city_name + ',' + country_name + '\n')
+for city in cities:
+    city_data['name'].append(city[0])
+    city_data['country_id'].append(city[1])
+
+city_file = pandas.DataFrame(city_data)
+city_file.to_csv('city.csv', index=False)
+
+# ----------------------------------------------------------------------------------------
