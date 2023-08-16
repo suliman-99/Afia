5f65eb7ccc74bf3abc05ec466c363a92878d13b1
Suliman Awad <sulimanawadstudy@gmail.com>
2023-08-16 15:55:57 +0300

    upgrade user creating logic

--- Auth/models.py
+++ Auth/models.py
@@ -1,4 +1,3 @@
-from typing import Iterable, List, Optional, Sequence
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager as BaseUserManager, Group as BaseGroup
